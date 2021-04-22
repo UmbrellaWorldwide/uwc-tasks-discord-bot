0cdbaf52ce29caeb58088aa15056411f37802c83
Edward Navarro <contacto@edwardnavarro.com>
2021-04-22 00:45:08 -0500

    Clean code

--- index.js
+++ index.js
@@ -28,57 +28,42 @@ client.on('message', message => {
 	const command = args.shift().toLowerCase();
 
 	if (command === 'server-info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
-		message.channel.send(`This server's name is: ${message.guild.name}`);
-	}
-	else if (command === 'server-extras') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+		// if (!args.length) {
+		// 	return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		// }
+		message.channel.send(`Server name: ${message.guild.name}`);
 	}
 	else if (command === 'user-info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	}
 	else if (command === 'channel-info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
 		message.channel.send(`Channel name: ${message.channel.name}\nChannel ID: ${message.channel.id}`);
 	}
 });
 
 
 // ===========> Start: Express server
-const data = '{"example":{"online":true,"status":"200"}}';
+const data = '{"uwc_tasks":{"online":true,"status":"200"}}';
 
 app.get('/', function(req, res) {
-    res.send('<html><head></head><body>UW Tasks</body></html>');
-    // res.send('<html><head><script>window.location.href = \'https://google.com/\'</script></head><body>You shouldn\'t be here! <a href=\'https://google.com\'>Exit</a></body></html>');
-    /** Redirect the browser to google with window.location.href
-     *  Change this to your site */
+	res.send('<html><head></head><body>UW Tasks</body></html>');
+	// res.send('<html><head><script>window.location.href = \'https://google.com/\'</script></head><body>You shouldn\'t be here! <a href=\'https://google.com\'>Exit</a></body></html>');
 });
 
 app.post('/', function(req, res) {
-    /** You could add some auth code here but
-     *  if your sending it all to the client there isn't much of a difference
-     *  because people could read it from the website. */
 	console.log(req);
-    res.type('json');
-    res.json(data);
-    /* Webserver --> Bot */
-    res.end();
+	res.type('json');
+	res.json(data);
+	/* Webserver --> Bot */
+	res.end();
 });
 
 app.post('/notify/send', function(req, res) {
 	console.log(req.body);
 	res.type('json');
 	res.json(data);
+	const channel = client.channels.cache.get(req.body.channel);
+	channel.send('<content>');
 	res.end();
 });
 
